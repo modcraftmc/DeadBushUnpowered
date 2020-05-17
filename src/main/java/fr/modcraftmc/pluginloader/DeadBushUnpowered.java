a874fd72bd3046514edfddcd022e57320f3fe55f
modcraftmc <contact@modcraftmc.fr>
2020-05-17 16:44:20 +0200

    Forge setup

---
+++ src/main/java/fr/modcraftmc/pluginloader/DeadBushUnpowered.java
@@ -0,0 +1,12 @@
+package fr.modcraftmc.pluginloader;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("deadbushunpowered")
+public class DeadBushUnpowered {
+
+    public DeadBushUnpowered() {
+
+    }
+
+}
